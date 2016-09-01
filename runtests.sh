5743e149a40ab6d2887fb690a00bb00d4c560420
Ragnar Lonn <ragnar@loadimpact.com>
2016-09-01 19:45:04 +0200

    all tests run except Gatling

--- runtests.sh
+++ runtests.sh
@@ -18,8 +18,8 @@ replace() {
   FNAME=$1
   STR=$2
   REPLACE=$3
-  ${AWK} -v rep="${REPLACE}" '{gsub("'${STR}'", rep);print $0}' >/tmp/_replace.tmp
-  mv -f /tmp/replace.tmp ${FNAME}
+  ${AWK} -v rep="${REPLACE}" '{gsub("'${STR}'", rep);print $0}' ${FNAME} >/tmp/_replace.tmp
+  mv -f /tmp/_replace.tmp ${FNAME}
 }
 
 # replace_all $source_cfg $target_cfg
@@ -43,82 +43,105 @@ replace_all() {
 
 # Static-URL tests
 apachebench_static() {
+  echo ""
+  echo "apachebench_static: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/apachebench/static
   mkdir -p ${RESULTS}
-  echo ""
-  echo "Executing ab -k -t ${CONCURRENT} -n ${REQUESTS} -c ${CONCURRENT} ${TARGETURL} ... "
+  echo "apachebench_static: Executing ab -k -t ${CONCURRENT} -n ${REQUESTS} -c ${CONCURRENT} ${TARGETURL} ... "
   ab -k -t ${CONCURRENT} -n ${REQUESTS} -c ${CONCURRENT} ${TARGETURL} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "apachebench_static: done"
 }
 boom_static() {
+  echo ""
+  echo "boom_static: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/boom/static
   mkdir -p ${RESULTS}
-  echo ""
-  echo "Executing boom -n ${REQUESTS} -c ${CONCURRENT} ${TARGETURL} ... "
+  echo "boom_static: Executing boom -n ${REQUESTS} -c ${CONCURRENT} ${TARGETURL} ... "
   boom -n ${REQUESTS} -c ${CONCURRENT} ${TARGETURL} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "boom_static: done"
 }
 wrk_static() {
+  echo ""
+  echo "wrk_static: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/wrk/static
   mkdir -p ${RESULTS}
-  echo ""
-  echo "Executing wrk -c ${CONCURRENT} -t ${CONCURRENT} -d ${DURATION} ${TARGETURL} ... "
+  echo "wrk_static: Executing wrk -c ${CONCURRENT} -t ${CONCURRENT} -d ${DURATION} ${TARGETURL} ... "
   ${TESTDIR}/wrk/wrk -c ${CONCURRENT} -t ${CONCURRENT} -d ${DURATION} ${TARGETURL} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "wrk_static: done"
 }
 artillery_static() {
+  echo ""
+  echo "artillery_static: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/artillery/static
   REQS_PER_VU=`expr ${REQUESTS} \/ ${CONCURRENT}`
   mkdir -p ${RESULTS}
-  echo ""
-  echo "Executing artillery quick --count ${CONCURRENT} -n ${REQS_PER_VU} ${TARGETURL} ... "
+  echo "artillery_static: Executing artillery quick --count ${CONCURRENT} -n ${REQS_PER_VU} ${TARGETURL} ... "
   artillery quick --count ${CONCURRENT} -n ${REQS_PER_VU} ${TARGETURL} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "artillery_static: done"
 }
 vegeta_static() {
+  echo ""
+  echo "vegeta_static: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/vegeta/static
   # Vegeta only supports static request rates. You might want to change the REQUESTS parameter until you get the highest throughput w/o errors.
-  RATE=`expr ${REQUESTS} \/ ${DURATION}`
+  RATE=`expr ${REQUESTS} \/ ${DURATION}`
   mkdir -p ${RESULTS}
-  echo ""
-  echo "Executing echo \"GET ${TARGETURL}\" vegeta attack -rate=${RATE} -connections=${CONCURRENT} -duration=${DURATION}s ... "
+  echo "vegeta_static: Executing echo \"GET ${TARGETURL}\" vegeta attack -rate=${RATE} -connections=${CONCURRENT} -duration=${DURATION}s ... "
   echo "GET ${TARGETURL}" |vegeta attack -rate=${RATE} -connections=${CONCURRENT} -duration=${DURATION}s |vegeta report > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "vegeta_static: done"
 }
 siege_static() {
+  echo ""
+  echo "siege_static: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/siege/static
   mkdir -p ${RESULTS}
-  echo ""
-  echo "Executing siege -b -t ${DURATION}S -q -c ${CONCURRENT} ${TARGETURL} ... "
+  echo "siege_static: Executing siege -b -t ${DURATION}S -q -c ${CONCURRENT} ${TARGETURL} ... "
   siege -b -t ${DURATION}S -q -c ${CONCURRENT} ${TARGETURL} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  # Siege has pretty crappy logging options. Doesnt seem possible to tell it where to write its logfile
+  mv -f /var/log/siege.log ${RESULTS}
+  echo "siege_static: done"
 }
 tsung_static() {
+  echo ""
+  echo "tsung_static: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/tsung/static
   mkdir -p ${RESULTS}
   CFG=${TESTDIR}/configs/tsung_${STARTTIME}.xml
   replace_all ${TESTDIR}/configs/tsung.xml ${CFG}
-  echo ""
-  echo "Executing tsung -l ${RESULTS} -f ${CFG} start ... "
+  echo "tsung_static: Executing tsung -l ${RESULTS} -f ${CFG} start ... "
   tsung -l ${RESULTS} -f ${CFG} start > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "tsung_static: done"
 }
 jmeter_static() {
+  echo ""
+  echo "jmeter_static: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/jmeter/static
   mkdir -p ${RESULTS}
   CFG=${TESTDIR}/configs/jmeter_${STARTTIME}.xml
   # TODO: support for protocols other than plain HTTP... we dont specify protocol in the test plan ATM
   replace_all ${TESTDIR}/configs/jmeter.xml ${CFG}
-  echo ""
-  echo "Executing jmeter -n -t ${CFG} ... "
+  echo "jmeter_static: Executing jmeter -n -t ${CFG} ... "
   ${TESTDIR}/apache-jmeter-3.0/bin/jmeter -n -t ${CFG} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "jmeter_static: done"
 }
 
 # Scripting tests
 locust_scripting() {
+  echo ""
+  echo "locust_scripting: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/locust/static
   mkdir -p ${RESULTS}
-  CFG=${TESTDIR}/configs/blocust_${STARTTIME}.py
+  CFG=${TESTDIR}/configs/locust_${STARTTIME}.py
   # TODO: we only support plain HTTP here also
-  replace_all ${TESTDIR}/configs/locust.py >${CFG}
-  echo ""
-  echo "Executing locust --host=\"http://${HOST}\" --locustfile=${CFG} --no-web --clients=${CONCURRENT} --hatch-rate=${CONCURRENT} --num-request=${REQUESTS} ... "
-  locust --host="http://${HOST}" --locustfile=${CFG} --no-web --clients=${CONCURRENT} --hatch-rate=${CONCURRENT} --num-request=${REQUESTS} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  replace_all ${TESTDIR}/configs/locust.py ${CFG}
+  TARGETHOST=`echo ${TARGETURL} |${SED} 's/https:\/\///' |${SED} 's/http:\/\///' |cut -d\/ -f1`
+  echo "locust_scripting: Executing locust --host=\"http://${TARGETHOST}\" --locustfile=${CFG} --no-web --clients=${CONCURRENT} --hatch-rate=${CONCURRENT} --num-request=${REQUESTS} ... "
+  locust --host="http://${TARGETHOST}" --locustfile=${CFG} --no-web --clients=${CONCURRENT} --hatch-rate=${CONCURRENT} --num-request=${REQUESTS} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "locust_scripting: done"
 }
 grinder_scripting() {
+  echo ""
+  echo "grinder_scripting: starting at "`date +%y%m%d-%H:%M:%S`
   export RESULTS=${TESTDIR}/results/${STARTTIME}/grinder/static
   mkdir -p ${RESULTS}
   CFG=${TESTDIR}/configs/grinder_${STARTTIME}.py
@@ -129,30 +152,36 @@ grinder_scripting() {
   # Grinder specifies thread duration in ms
   GRINDER_DURATION=`expr ${DURATION} \* 1000`
   replace $TMPCFG2 "DURATION" "${GRINDER_DURATION}"
+  replace $TMPCFG2 "SCRIPT" "${CFG}"
   replace_all $TMPCFG2 $CFG2
   rm $TMPCFG2
   cd ${TESTDIR}/configs
-  echo ""
-  echo "Executing java net.grinder.Grinder ${CFG2} ... "
+  export CLASSPATH=/loadgentests/grinder-3.11/lib/grinder.jar
+  echo "grinder_scripting: Executing java net.grinder.Grinder ${CFG2} ... "
   java net.grinder.Grinder ${CFG2} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "grinder_scripting: done"
 }
 gatling_scripting() {
+  echo ""
+  echo "gatling_scripting: starting at "`date +%y%m%d-%H:%M:%S`
   RESULTS=${TESTDIR}/results/${STARTTIME}/gatling/static
   mkdir -p ${RESULTS}
   CFG=${TESTDIR}/configs/gatling_${STARTTIME}.scala  
   replace_all ${TESTDIR}/configs/gatling.scala ${CFG}
-  echo ""
-  echo "Executing gatling ... "
+  echo "gatling_scripting: Executing gatling ... "
   # TODO - fix Gatlings tricky config setup...  
+  echo "gatling_scripting: done"
 }
 wrk_scripting() {
+  echo ""
+  echo "wrk_scripting: starting at "`date +%y%m%d-%H:%M:%S`
   RESULTS=${TESTDIR}/results/${STARTTIME}/wrk/static
   mkdir -p ${RESULTS}
   CFG=${TESTDIR}/configs/wrk_${STARTTIME}.lua
   replace_all ${TESTDIR}/configs/wrk.lua ${CFG}
-  echo ""
-  echo "Executing wrk -c ${CONCURRENT} -t ${CONCURRENT} -d ${DURATION} --script ${CFG} ... "
-  ${TESTDIR}/wrk/wrk -c ${CONCURRENT} -t ${CONCURRENT} -d ${DURATION} --script ${CFG} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "wrk_scripting: Executing wrk -c ${CONCURRENT} -t ${CONCURRENT} -d ${DURATION} --script ${CFG} ${TARGETURL} ... "
+  ${TESTDIR}/wrk/wrk -c ${CONCURRENT} -t ${CONCURRENT} -d ${DURATION} --script ${CFG} ${TARGETURL} > >(${TEE} ${RESULTS}/stdout.log) 2> >(${TEE} ${RESULTS}/stderr.log >&2)
+  echo "wrk_scripting: done"
 }
 
 staticurltests() {
