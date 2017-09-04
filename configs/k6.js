08832981e0d69f53f4ae1c263d1828068f721081
Ragnar Lonn <ragnar@loadimpact.com>
2017-09-04 12:23:14 +0200

    Do not use JSON output - parsing too slow.

--- configs/k6.js
+++ configs/k6.js
@@ -1,7 +1,7 @@
 import http from "k6/http";
 
 export default function() {
-  for (var i = 0; i < REQS_PER_VU; i++) {
+  for (var i = 0; i <= (REQS_PER_VU/100); i++) {
     http.get("TARGETURL");
   }
 };
